36f402b0718df5dd497c19069f4f934891a9795d
alistairisjesus <38551960+alistairisjesus@users.noreply.github.com>
2019-06-01 12:49:10 +1200

    merging

--- index.js
+++ index.js
@@ -3,25 +3,31 @@ $(document).ready(function() {
   var eventList = [
     {
       Name: "gig",
-      Description: "giggity goo"
+      Date: "giggity goo",
+      Location: "Wellywood",
+      Description: "We gon' gig",
+      Author: "Ali G"
     },
     {
-      Name: "Elk",
-      Description: "Valhalla"
+      Name: "Party",
+      Date: "Tonight!",
+      Location: "At Laura's friend's house",
+      Description: "We gon' gig it upppp",
+      Author: "Moi"
     }
   ];
 
 
 
     $(eventList).each(function( index ) {
-
-      var elm = document.getElementById('divTarget');
-      var node = document.createElement("div");
-
-
-      var textnode = document.createTextNode(eventList[index].Name + eventList[index].Description);
-      node.appendChild(textnode);
-      elm.append(node);
+      var elm = document.getElementById('tableRows');
+      var row = document.createElement("tr");
+      for (var i=0; i<eventList[0].length; i++) {
+        var attribute = createElement("td");
+        var textnode = document.createTextNode(eventList[index].Name);
+        attribute.appendChild(textNode);
+      }
+      elm.appendChild(row);
       console.log(elm);
 
 
