3d3077fc26da59a0df0b7cfd0dfe951362de658a
alistairisjesus <38551960+alistairisjesus@users.noreply.github.com>
2019-06-01 14:01:03 +1200

    Added events to table

--- index.js
+++ index.js
@@ -1,34 +1,59 @@
 $(document).ready(function() {
 
+  var eventDisplayInfo = ["Name", "Date", "Location", "Description", "Author"];
+
   var eventList = [
+    {
+      Name: "FluxJam",
+      Date: "01/06/19",
+      Location: "Flux Federation",
+      Description: "",
+      Author: "Ali G",
+      Category: "Music",
+      TimeStamp: "Fri Jul 17 1998 00:00:00 GMT+1200 (New Zealand Standard Time)"
+    },
     {
       Name: "gig",
       Date: "giggity goo",
       Location: "Wellywood",
       Description: "We gon' gig",
-      Author: "Ali G"
+      Author: "Ali G",
+      Category: "Music",
+      TimeStamp: "Fri Jul 17 1998 00:00:00 GMT+1200 (New Zealand Standard Time)"
+    },
+    {
+      Name: "gig",
+      Date: "giggity goo",
+      Location: "Wellywood",
+      Description: "We gon' gig",
+      Author: "Ali G",
+      Category: "Music",
+      TimeStamp: "Fri Jul 17 1998 00:00:00 GMT+1200 (New Zealand Standard Time)"
     },
     {
       Name: "Party",
       Date: "Tonight!",
       Location: "At Laura's friend's house",
       Description: "We gon' gig it upppp",
-      Author: "Moi"
+      Author: "Moi",
+      Category: "Music",
+      TimeStamp: "Fri Jul 17 1998 00:00:00 GMT+1200 (New Zealand Standard Time)"
     }
   ];
 
 
-
     $(eventList).each(function( index ) {
-      var elm = document.getElementById('tableRows');
+      var tbody = document.getElementById('tableRows');
       var row = document.createElement("tr");
-      for (var i=0; i<eventList[0].length; i++) {
-        var attribute = createElement("td");
-        var textnode = document.createTextNode(eventList[index].Name);
+
+      for (var i=0; i<eventDisplayInfo.length; i++) {
+        var attribute = document.createElement("td");
+        var textNode = document.createTextNode(eventList[index][eventDisplayInfo[i]]);
         attribute.appendChild(textNode);
+        row.append(attribute);
       }
-      elm.appendChild(row);
-      console.log(elm);
+
+      tbody.appendChild(row);
 
 
       //
