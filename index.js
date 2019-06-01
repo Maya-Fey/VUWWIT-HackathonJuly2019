b4b0c1fbd33b6b989592457a6843e8ee4631301c
alistairisjesus <38551960+alistairisjesus@users.noreply.github.com>
2019-06-01 12:23:34 +1200

    Set up repeating divs

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-import angular from 'angular';
+$(document).ready(function() {
 
-angular.module('StudentBulletin', []).controller('MainCtrl', function($scope, $http) {
+  var eventList = [
+    {
+      Name: "gig",
+      Description: "giggity goo"
+    },
+    {
+      Name: "Elk",
+      Description: "Valhalla"
+    }
+  ];
 
+
+
+    $(eventList).each(function( index ) {
+
+      var elm = document.getElementById('divTarget');
+      var node = document.createElement("div");
+
+
+      var textnode = document.createTextNode(eventList[index].Name + eventList[index].Description);
+      node.appendChild(textnode);
+      elm.append(node);
+      console.log(elm);
+
+
+      //
+      // $("#divTarget").append(node);
+      // console.log(index);
+  });
 });
