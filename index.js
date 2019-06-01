20bc486dbc95634aaf35ee41b6664d66b874615b
alistairisjesus <38551960+alistairisjesus@users.noreply.github.com>
2019-06-01 16:20:29 +1200

    Refreshed page after filtering

--- index.js
+++ index.js
@@ -5,54 +5,17 @@ var eventDisplayInfo = ["TITLE", "DATETIME", "LOCATION", "DESCRIPTION", "AUTHOR"
 
 $(document).ready(function() {
 
-  getBulletinsNoCategory((a) => {
-    eventList = a;
-    console.log(a);
-    console.log(eventList.data[0].AUTHOR)
-    loadTable();
-  });
+  getBulletinsNoCategory();
+
 });
 
 
-  //   [{
-  //     Name: "FluxJam",
-  //     Date: "01/06/19",
-  //     Location: "Flux Federation",
-  //     Description: "",
-  //     Author: "Ali G",
-  //     Category: "Music"
-  //   },
-  //   {
-  //     Name: "gig",
-  //     Date: "giggity goo",
-  //     Location: "Wellywood",
-  //     Description: "We gon' gig",
-  //     Author: "Ali G",
-  //     Category: "Music"
-  //   },
-  //   {
-  //     Name: "gig",
-  //     Date: "giggity goo",
-  //     Location: "Wellywood",
-  //     Description: "We gon' gig",
-  //     Author: "Ali G",
-  //     Category: "Music"
-  //   },
-  //   {
-  //     Name: "Party",
-  //     Date: "Tonight!",
-  //     Location: "At Laura's friend's house",
-  //     Description: "We gon' gig it upppp",
-  //     Author: "Moi",
-  //     Category: "Event"
-  //   }
-  // ];
-
 const loadTable = () => {
+  // Reset table
+  var tbody = document.getElementById("tableRows");
+  tbody.innerHTML = '';
 
   eventList.data.map((event, index) => {
-
-    console.log(index);
     var tbody = document.getElementById('tableRows');
     var row = document.createElement("tr");
 
@@ -61,7 +24,6 @@ const loadTable = () => {
       var textNode = document.createTextNode(event[eventDisplayInfo[i]]);
       attribute.appendChild(textNode);
       row.append(attribute);
-      console.log(attribute);
     }
 
     tbody.appendChild(row);
@@ -70,44 +32,52 @@ const loadTable = () => {
 
 
 const submitFilters = () => {
-  console.log(selectedCategory);
-  console.log(sortBy);
+  getBulletins(selectedCategory, (events)=>{
+    eventList = events;
+    loadTable();
+  });
 
 }
 
+getBulletinsNoCategory((a) => {
+  eventList = a;
+  loadTable();
+});
+
 const submitNewBullet = () => {
+  var category = "";
+  var radios = document.getElementsByName('Category');
+
+  for (var i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+      category = radios[i].value;
+      break;
+    }
+  }
+
   var bullet = {
     Name: document.getElementById("bulletName").value,
     Date: document.getElementById("bulletDate").value,
     Location: document.getElementById("bulletLocation").value,
     Description: document.getElementById("bulletDescription").value,
     Author: "user1",
-    Category: document.getElementById("bulletName").value
-
+    Category: category
   }
 
   const { Name, Date, Location, Description, Author, Category} = bullet;
-
   newBulletin(Name, Description, Category, Location, Date, (a,b) => {});
-
   $('#exampleModal').modal(show=false);
+
 }
 
 const updateCategory = (category) => {
-  console.log(category);
   selectedCategory = category
-  console.log();
 }
 
 const updateSort = (type) => {
-  console.log(type);
   sortBy = type
 }
 
-const submit = () => {
-
-  //hit api
-}
 
 const checkIfLoggedIn = () => {
   isLoggedIn()
